f8a2cee8c61f649ab45556e01a80c462658ea2ea
Daniel Viola <daniel.viola001@mymdc.net>
2023-02-15 14:47:50 -0500

    Restructuring folders

--- Calzini/Program.cs
+++ Calzini/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calzini.Core;
+using System;
 using System.Threading.Tasks;
 
 namespace Calzini
--- Calzini/Source/Core/TimeFormat.cs
+++ Calzini/Source/Core/TimeFormat.cs
@@ -1,6 +1,6 @@
 ﻿using System.Globalization;
 
-namespace Calzini
+namespace Calzini.Core
 {
     public class TimeFormat
     {
